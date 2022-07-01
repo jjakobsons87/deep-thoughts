4d1bab1b480b2d44d9c5ba6cc7a07ad91c2f5a93
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-07-01 16:35:17 -0400

    update to add reactions to query

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -10,9 +10,17 @@ const typeDefs = gql`
         createdAt: String
         username: String
         reactionCount: Int
+        reactions: [Reaction]
     }
 
-type Query {
+    type Reaction {
+        _id: ID
+        reactionBody: String
+        createdAt: String
+        username: String
+    }
+
+    type Query {
         thoughts(username: String): [Thought]
     }
 `;
