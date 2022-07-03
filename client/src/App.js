2f709ea2fec093c82709de37a19570d4be9b0551
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-07-03 10:53:33 -0400

    update routes to handle NoMatch

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import NoMatch from './pages/NoMatch';
 import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
 import Signup from './pages/Signup';
 
 const httpLink = createHttpLink({
@@ -51,6 +52,10 @@ function App() {
               <Route 
                 path='/thought'
                 element={<SingleThought />}
+              />
+              <Route
+                path='*'
+                element={<NoMatch />}
               />
             </Routes>
           </div>
