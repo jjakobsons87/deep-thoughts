b5d204cab8bcfa4cc560b2897fdaa1c26f9fa951
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-07-02 22:28:09 -0400

    addReaction mutation in resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -74,6 +74,17 @@ const resolvers = {
                 return thought;
             }
             throw new AuthenticationError('You need to be logged in!');
+        },
+        addReaction: async (parent, { thoughtId, reactionBody }, context) => {
+            if (context.user) {
+                const updatedThought = await Thought.findOneAndUpdate(
+                    { _id: thoughtId },
+                    { $push: { reactions: { reactionBody, username: context.user.username } } },
+                    { new: true, runValidators: true }
+                );
+                return updatedThought;
+            }
+            throw new AuthenticationError('You need to be logged in!');
         }
     }
 };
