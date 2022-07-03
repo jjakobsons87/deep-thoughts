4f4a0eca628c10044a91748fa9c12851f9ab8e0a
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-07-02 22:05:26 -0400

    fix add user, me query returning same username/email

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -44,7 +44,7 @@ const resolvers = {
             const user = await User.create(args);
             const token = signToken(user);
 
-            return user;
+            return { token, user };
         },
         login: async (parent, { email, password }) => {
             const user = await User.findOne({ email });
