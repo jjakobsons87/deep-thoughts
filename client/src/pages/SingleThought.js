aaa0b06af43fb5240b2695b6c70bc750b3ece16c
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-07-03 14:50:51 -0400

    add single thought query

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import { QUERY_THOUGHT } from '../utils/queries';
 
 const SingleThought = props => {
   const { id: thoughtId } = useParams();
-  console.log(thoughtId);
   
+  const { loading, data } = useQuery(QUERY_THOUGHT, {
+    variables: { id: thoughtId }
+  });
+
+  const thought = data?.thought || {};
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
       <div className="card mb-3">
