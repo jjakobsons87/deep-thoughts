8e4fe7f0a825338456644ef8ef097e03217f986d
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-07-03 14:45:42 -0400

    add linking to profile and single thought :

--- client/src/App.js
+++ client/src/App.js
@@ -45,12 +45,13 @@ function App() {
                 path='/signup'
                 element={<Signup />}
               />
+              <Route path= "/profile">
+                <Route path=":username" element={<Profile />} />
+                <Route path="" element={<Profile />} />
+              </Route>
+                
               <Route 
-                path='/profile'
-                element={<Profile />}
-              />
-              <Route 
-                path='/thought'
+                path="/thought/:id"
                 element={<SingleThought />}
               />
               <Route
