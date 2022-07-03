e117b900ba7e146230953815d8c912865a652ad6
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-07-02 23:26:18 -0400

    update client

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,9 @@
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+// ApolloProvider - special component that will provide data to all the other components 
+// ApolloClient - constructor function to help initialize the connection to GraphQL API server 
+// InMemoryCache - enables Apollo Client to cache API response data 
+// createHttpLink - control how Apollo Client makes a request (middleware for outbound network requests)
+
 import React from 'react';
 
 import Header from './components/Header';
@@ -5,15 +11,26 @@ import Footer from './components/Footer';
 
 import Home from './pages/Home';
 
+const httpLink = createHttpLink({
+  uri: 'http://localhost:3001/graphql',
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <div className='flex-column justify-flex-start min-100-vh'>
-      <Header />
-      <div className='container'>
-        <Home />
+    <ApolloProvider client={client}>
+      <div className="flex-column justify-flex-start min-100-vh">
+        <Header />
+        <div className="container">
+          <Home />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </ApolloProvider>
   );
 }
 
