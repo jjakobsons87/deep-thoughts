f05af9407ab4cdefa27005e6f5bc848b2a2bf940
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-07-04 17:51:24 -0400

    remove add friend from own profile

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -48,16 +48,21 @@ const Profile = () => {
     <div>
       <div className="flex-row mb-3">
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
-          Viewing {userParam ? `${user.username}'s` : 'your'} profile.
+          Viewing {userParam ? `${user.username}'s` : "your"} profile.
         </h2>
-        <button className='btn ml-auto' onClick={handleClick}>
-          Add Friend
-        </button>
+        {userParam && (
+          <button className="btn ml-auto" onClick={handleClick}>
+            Add Friend
+          </button>
+        )}
       </div>
 
       <div className="flex-row justify-space-between mb-3">
         <div className="col-12 mb-3 col-lg-8">
-          <ThoughtList thoughts={user.thoughts} title={`${user.username}'s thoughts...`} />
+          <ThoughtList
+            thoughts={user.thoughts}
+            title={`${user.username}'s thoughts...`}
+          />
         </div>
 
         <div className="col-12 col-lg-3 mb-3">
